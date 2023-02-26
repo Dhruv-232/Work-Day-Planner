60722bbbd90a78e6ba06d666e37f19226c4a9346
Dhruv-232 <dhruvsharma2004@outlook.com>
2023-02-26 17:28:44 +1100

    Added the time blocks for each time slot between 9 to 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,39 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+$(document).ready(function() {
+    // get current day and time using dayjs library
+    var currentDate = dayjs().format("dddd, MMMM D");
+    var currentTime = dayjs().format("hA");
+  
+    // update header with current day and time
+    $("#currentDay").text("Today is " + currentDate + " and it's currently " + currentTime);
+  
+    // loop through each time-block div and update its background color based on whether it's in the past, present, or future
+    $(".time-block").each(function() {
+      // get hour value from div id and convert to 24-hour format
+      var hour = parseInt($(this).attr("id").split("-")[1]);
+  
+      if (hour < dayjs().hour()) {
+        $(this).removeClass("present future").addClass("past");
+      }
+      else if (hour === dayjs().hour()) {
+        $(this).removeClass("past future").addClass("present");
+      }
+      else {
+        $(this).removeClass("past present").addClass("future");
+      }
+    });
+  
+    // save button click event listener
+    $(".saveBtn").on("click", function() {
+      // get textarea value and its corresponding hour
+      var text = $(this).siblings(".description").val().trim();
+      var hour = $(this).parent().attr("id");
+  
+      // save text to local storage using hour as key
+      localStorage.setItem(hour, text);
+    });
+  
+    // load saved text from local storage
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
   });
-    
